f40963cf8bcef926319ef89a80b6f99efa9b853b
jaehyun <jaehyun@smartm2m.co.kr>
2025-11-11 09:46:35 +0900

    공격 2종 추가

--- backend/app/ai/attacks/evasion/noise_osfd.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e0a816934de204f6600c3b9abb0603cd79e803de67d230d3be0726d8df5e3296
-size 20182
--- backend/app/ai/attacks/evasion/universal_noise.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:40a370112e3604c185c7628e8777ca2ab06f66b559e1febfbcf7675a244bc62b
-size 11132
